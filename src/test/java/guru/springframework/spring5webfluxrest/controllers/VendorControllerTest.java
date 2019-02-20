81d3b54ff5c54a53212a45f941c3cbe6f824b55d
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-02-20 00:30:38 -0500

    Adding POST to Vendor controller

--- src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
+++ src/test/java/guru/springframework/spring5webfluxrest/controllers/VendorControllerTest.java
@@ -6,9 +6,12 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.BDDMockito;
 import org.mockito.Mockito;
+import org.reactivestreams.Publisher;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import static org.mockito.ArgumentMatchers.any;
 
 /**
  * Created by REMI Networks
@@ -49,5 +52,21 @@ public class VendorControllerTest {
         webTestClient.get().uri("/api/v1/vendors/someid")
                 .exchange()
                 .expectBody(Vendor.class);
+    }
+
+    @Test
+    public void testCreateVendor() throws Exception {
+        BDDMockito.given(vendorRepository.saveAll(any(Publisher.class)))
+                .willReturn(Flux.just(Vendor.builder().build()));
+
+        Mono<Vendor> vendorToSaveMono = Mono.just(Vendor.builder().firstName("First Name")
+                                                 .lastName("Last Name").build());
+
+        webTestClient.post()
+                .uri("/api/v1/vendors")
+                .body(vendorToSaveMono, Vendor.class)
+                .exchange()
+                .expectStatus()
+                .isCreated();
     }
 }
