579170b2be1e6464f4d4fca6ef856e9a17a94b2a
Donald F. Coffin <donald.coffin@reminetworks.com>
2019-02-19 23:19:25 -0500

    Adding Vendor Controller and Controller Tests

--- src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
+++ src/main/java/guru/springframework/spring5webfluxrest/domain/Vendor.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * Created by REMI Networks
@@ -12,13 +14,13 @@ import lombok.NoArgsConstructor;
  * Time: 7:17 PM
  **/
 @Data
-//@Document
+@Document
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Vendor {
 
-//    @Id
+    @Id
     private String id;
 
     private String firstName;
